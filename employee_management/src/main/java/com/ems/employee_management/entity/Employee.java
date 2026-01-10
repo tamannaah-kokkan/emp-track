e5d4c4bf0e7c62c69309250185b659e58f194235
tamannaah-kokkan <tamannaah1302@gmail.com>
2026-01-10 09:53:15 +0530

    Initial Commit

---
+++ employee_management/src/main/java/com/ems/employee_management/entity/Employee.java
@@ -0,0 +1,27 @@
+package com.ems.employee_management.entity;
+
+import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data //used to add getters and setters automatically using lombok dependency
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id; //Database primary key
+	
+	private int employeeId; //eg: EMP101
+	private String name;
+	private String email;
+	private int phone;
+	private String department;
+	private String designation;
+	private double salary;
+	private LocalDate dateOfJoining;
+	
+}
