97d11d7ab13e6c5c607609ed0d64ba0ceb62c470
tamannaah-kokkan <tamannaah1302@gmail.com>
2026-04-25 21:37:51 +0530

    Footer component  and routing started

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import { ListEmployee } from './components/ListEmployee'
 
 function App() {
   return (
     <>
+
+    <BrowserRouter>
+
       <Header></Header>
-      <ListEmployee></ListEmployee>
+
+      <Routes>
+        <Route path="/" element = {<ListEmployee/>}></Route>
+        <Route path="/employees" element = {<ListEmployee/>}></Route>
+      </Routes>
+
+      <Footer></Footer>
+    </BrowserRouter>
+
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white p-4 mt-8">
+            <div className="max-w-7xl">
+                <p className="text-center text-sm">© 2026 EmployeeTrack. All rights reserved.</p>
+            </div>
+        </footer>
+    )
+}
